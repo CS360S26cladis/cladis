c3c49af1be3834f69eb710bce0730c40a8f0d620
Maaz Shahid <129919953+KarmaMS@users.noreply.github.com>
2026-02-17 15:51:33 +0500

    Add blue color field to Shape

--- app/src/main/java/com/example/cladis/Shape.java
+++ app/src/main/java/com/example/cladis/Shape.java
@@ -3,6 +3,7 @@ package com.example.cladis;
 public abstract class Shape {
     Integer x;
     Integer y;
+    String color = "magenta";
 
     public Shape(Integer x, Integer y) {
         this.x = x;
