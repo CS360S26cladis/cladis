b1a630461f2a720101ced9f78282cb93139a1264
Mowahid Imran <mowahid923@gmail.com>
2026-02-17 13:30:14 +0500

    Abstract Shape Class

--- app/src/main/java/com/example/cladis/Shape.java
+++ app/src/main/java/com/example/cladis/Shape.java
@@ -1,4 +1,11 @@
 package com.example.cladis;
 
-public class Shape {
+public abstract class Shape {
+    Integer x;
+    Integer y;
+
+    public Shape(Integer x, Integer y) {
+        this.x = x;
+        this.y = y;
+    }
 }
