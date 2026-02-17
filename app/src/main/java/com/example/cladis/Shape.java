cb2e9d4101cc44a694acc233ff73e5a86d569218
laalbaig <amaanbaig189@gmail.com>
2026-02-17 14:31:03 +0500

    amaanbaigColor

--- app/src/main/java/com/example/cladis/Shape.java
+++ app/src/main/java/com/example/cladis/Shape.java
@@ -3,9 +3,11 @@ package com.example.cladis;
 public abstract class Shape {
     Integer x;
     Integer y;
+    String color;
 
-    public Shape(Integer x, Integer y) {
+    public Shape(Integer x, Integer y,String color) {
         this.x = x;
         this.y = y;
+        this.color = color;
     }
 }
